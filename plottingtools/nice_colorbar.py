c523ab9da9c5957238e2d5e64c6a4a31a80beba2
LauraOlivera <lauraoliverant@gmail.com>
2020-06-04 12:50:41 +0200

    add nice colorbar tool comments

--- plottingtools/nice_colorbar.py
+++ plottingtools/nice_colorbar.py
@@ -2,6 +2,15 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def nice_colorbar(im,ax,side="right",size="5%",pad=0.05):
+    """Make colorbars when using plt.imshow() that look nice
+	(see https://github.com/matplotlib/matplotlib/issues/17559)
+	Inputs:
+		im = plt.imshow(you_data)
+		ax = axis of the figure
+		side = where you want the colorbar to go
+		size = proportional to the main axis dimensions
+		pad = distance to main figure """
+
     if side is "right" or side is "left":
         orientation = 'vertical'
     else:
